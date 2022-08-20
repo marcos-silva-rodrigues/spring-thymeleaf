381812ff28f521b602206486e7b5d49b0b4f6890
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-20 19:34:36 -0300

    feat: add model and repository

---
+++ src/main/java/com/silva/marcos/webapp/repository/CityRepository.java
@@ -0,0 +1,9 @@
+package com.silva.marcos.webapp.repository;
+
+import com.silva.marcos.webapp.model.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+}
