3edd3cda75f410b54911b2c5862d90081f31d47b
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-20 18:36:04 -0300

    feat: add attributes to pages

--- src/main/java/com/silva/marcos/webapp/controller/HelloWorldController.java
+++ src/main/java/com/silva/marcos/webapp/controller/HelloWorldController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/")
 public class HelloWorldController {
@@ -14,11 +17,21 @@ public class HelloWorldController {
     @GetMapping
     public ModelAndView index() {
         ModelAndView mv = new ModelAndView("/hello");
+        mv.addObject("name", "Fulano");
         return mv;
     }
 
     @GetMapping("/new")
     public String newPath(Model model) {
+        List<String> list = new ArrayList<String>();
+        list.add("Fulano 01");
+        list.add("Fulano 02");
+        list.add("Fulano 03");
+        list.add("Fulano 04");
+        list.add("Fulano 05");
+
+        model.addAttribute("name", "Fulano");
+        model.addAttribute("list", list);
         return "new";
     }
 
