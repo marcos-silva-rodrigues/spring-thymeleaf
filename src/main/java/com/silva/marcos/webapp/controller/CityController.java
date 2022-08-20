96c38f7a3fec42742616bffdf4bdd7b163189c45
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2022-08-20 19:40:33 -0300

    feat: controller and pages

---
+++ src/main/java/com/silva/marcos/webapp/controller/CityController.java
@@ -0,0 +1,28 @@
+package com.silva.marcos.webapp.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/city")
+public class CityController {
+
+    @GetMapping
+    public String index(Model model) {
+        return "cities/index";
+    }
+
+    @GetMapping("/new")
+    public String newCity(Model model) {
+        return "cities/new";
+    }
+
+    @GetMapping("/{id}")
+    public String editCity(@PathVariable("id") Long id, Model model) {
+        return "cities/edit";
+    }
+}
